bed08ae59a3b9d42124163b7bb1d13a4b7b21d49
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-01 18:59:00 -0500

    Create state and render moda, pass props to childn

--- newsie/src/App.js
+++ newsie/src/App.js
@@ -3,6 +3,8 @@ import React, {useState, useEffect} from 'react'
 import { goFetch } from './apiCalls';
 import NewsReel from './NewsReel';
 import './App.css';
+import Header from './Header';
+import Modal from './Modal';
 
 
 function App() {
@@ -12,14 +14,13 @@ function App() {
   const [us, setUs] = useState([])
   const [world, setWorld] = useState([])
   const [highlighted, setHighlighted] = useState(null)
+  const [modalOpen, setModalOpen] =useState(false)
 
   useEffect(() => {
     goFetch('science')
     .then(data => setScience(data.results))
     goFetch('home')
     .then(data => setHome(data.results))
-    // goFetch('art')
-    // .then(data => setArt(data.results))
     goFetch('us')
     .then(data => setUs(data.results))
     goFetch('world')
@@ -32,10 +33,14 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
+      {modalOpen && <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} highlighted={highlighted} />}
       <NewsReel 
       home={home}
       setHighlighted={setHighlighted}
+      setModalOpen={setModalOpen}
       />
+      
     </div>
   );
 }
