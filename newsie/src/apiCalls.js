20643f57b7192af64659e40b7c46e4d3c032e42f
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-01 18:59:55 -0500

    mpdify api calls to be dynamic

--- newsie/src/apiCalls.js
+++ newsie/src/apiCalls.js
@@ -6,9 +6,6 @@ const key = process.env.REACT_APP_API_KEY
 const goFetch = (type) => {
     return fetch(`https://api.nytimes.com/svc/topstories/v2/${type}.json?api-key=X3Uf8Qouu90Prs7jV8xmkoZxSFs9iyxv`)
     .then(response => response.json())
-    // .then(data => setScience(data.results))
 }
 
-
-// const scienceData = goFetch('science')
 export {goFetch}
