c7abcbf81ac694d1a58e12b97da945277fde0cb7
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-01 18:59:38 -0500

    Create and style header

---
+++ newsie/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './Newsie.png'
+
+function Header() {
+  return (
+      <div className='header'>
+          <img className="logo"  src={logo} />
+      </div>
+    
+  )
+}
+
+export default Header
