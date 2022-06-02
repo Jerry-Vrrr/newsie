00469fd5cc78fdc77ae83dfbb9275201c84c21d6
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-01 19:00:35 -0500

    create dynamic article

--- newsie/src/Article.js
+++ newsie/src/Article.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Article.css";
 
-export default function ({ item, setHighlighted }) {
+export default function ({ item, setHighlighted, id, setModalOpen }) {
+const handleClick = () => {
+    setHighlighted(item)
+    setModalOpen(true)
+}
+
   return (
-    <div className="article">
+    <div className="article" id={id}>
     <div className='title' >
       <h4>{item.title}</h4>
     </div>
-    <img className="image"  src={item.multimedia[0].url} />
+    <img id={id} onClick={() => handleClick()} className="image"  src={item.multimedia[0].url} />
   </div>
   );
 }
