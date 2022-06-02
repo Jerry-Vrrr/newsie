82b4306d90775f309c50411f9ed8d514e83bc235
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-01 19:00:57 -0500

    render articles with data

--- newsie/src/NewsReel.js
+++ newsie/src/NewsReel.js
@@ -2,9 +2,9 @@ import React from "react";
 import Article from "./Article";
 import "./NewsReel.css";
 
-export default function NewsReel({ home, setHighlighted }) {
+export default function NewsReel({ home, setHighlighted, setModalOpen }) {
   const homeMap = home.map((item, i) => (
-    <Article setHighlighted={setHighlighted} item={item} key={i} />
+    <Article  setModalOpen={setModalOpen} setHighlighted={setHighlighted} item={item} id={i} key={i} />
   ));
 
   return <div className="reel">{homeMap}</div>;
