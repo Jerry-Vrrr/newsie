f8483e47b2b5c143a4c098acf10efe684fa5cfd7
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-06-02 09:26:59 -0500

    Create dynamic fetch function for filter

--- newsie/src/App.js
+++ newsie/src/App.js
@@ -9,24 +9,20 @@ import Modal from './Modal';
 
 function App() {
 
-  const [science, setScience] = useState([])
   const [home, setHome] = useState([])
-  const [us, setUs] = useState([])
-  const [world, setWorld] = useState([])
   const [highlighted, setHighlighted] = useState(null)
   const [modalOpen, setModalOpen] =useState(false)
 
   useEffect(() => {
-    goFetch('science')
-    .then(data => setScience(data.results))
     goFetch('home')
     .then(data => setHome(data.results))
-    goFetch('us')
-    .then(data => setUs(data.results))
-    goFetch('world')
-    .then(data => setWorld(data.results))
   }, [] )
 
+  const filterFetch = (type) => {
+    goFetch(type)
+    .then(data => setHome(data.results))
+
+  }
  
   
   
